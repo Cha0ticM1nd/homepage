ae09a36d2023fb4dbdf6ce9a5770c903349ee23b
Magda Kosowska <magdakosowska32@gmail.com>
2023-06-30 23:59:33 +0200

    code organizing

--- js/script.js
+++ js/script.js
@@ -6,31 +6,33 @@
   const toggleTheme = () => {
     const body = document.querySelector(".body");
     const section = document.querySelectorAll(".section");
-    const sectionImageDescription = document.querySelectorAll(".js-imageDescription");
+    const sectionImageDescription = document.querySelectorAll(
+      ".js-imageDescription"
+    );
     const themeName = document.querySelector(".js-themeNameChange");
     const navigationLink = document.querySelectorAll(".js-navigationLink");
     const footerLink = document.querySelector(".js-footerLink");
     const footerText = document.querySelector(".js-footerText");
 
     body.classList.toggle("body--light");
-
     headerButton.classList.toggle("header__button--light");
+    footerLink.classList.toggle("footer__link--light");
+    footerText.classList.toggle("footer__text--dark");
+    section.forEach((section) => section.classList.toggle("section--dark"));
+
+    sectionImageDescription.forEach((sectionImageDescription) =>
+      sectionImageDescription.classList.toggle(
+        "section__imageDescription--dark"
+      )
+    );
+
+    navigationLink.forEach((navigationLink) =>
+      navigationLink.classList.toggle("navigation__link--light")
+    );
 
     themeName.innerText = body.classList.contains("body--light")
       ? "Jasny"
       : "Ciemny";
-
-    sectionImageDescription.forEach((sectionImageDescription) =>
-      sectionImageDescription.classList.toggle("section__imageDescription--dark")
-    );
-
-    section.forEach((section) => section.classList.toggle("section--dark"));
-
-    navigationLink.forEach((navigationLink) => navigationLink.classList.toggle("navigation__link--light"));
-
-    footerLink.classList.toggle("footer__link--light");
-
-    footerText.classList.toggle("footer__text--dark");
   };
 
   const headerButton = document.querySelector(".js-button");
@@ -43,4 +45,3 @@
 
   init();
 }
-
